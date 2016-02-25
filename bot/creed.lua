2fac2b949cfdf54e9d7fc9a30065f75fa6aa4738
ZACTELEGRAM1 <alirezasoltani13844@Gmail.com>
2016-02-25 21:17:28 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -234,19 +234,19 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {121663087},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
     
      Hello my Good friends 
      
-    ‼️ this bot is made by : @creed_is_dead
+    ‼️ this bot is made by : @EAMALIBOTONLAIN
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @SOLTANI330
+   ߔࠀ   @SOLTANI330
+   ߔࠀ   @SOLTANI330
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
@@ -360,14 +360,14 @@ function create_config( )
 ------------------------------------------------------------------------------------------------------------
 
 
-کانال ما : @Pro_Programing
+کانال ما : @EMAMALI_PRO
 
 اعضای اصلی ما : 
 
-@Arsalan_Sahel  [سازنده]
-@Bad00boy_Creed [عضو اصلی ]
-@Creed_aria [عضو اصلی]
-@AmirCc_CreeD [عضو اصلی]
+@SOLTANI330  [سازنده]
+@SOLTANI330 [عضو اصلی]
+@SOLTANI330 [عضو اصلی]
+@SOLTANI330 [عضو اصلی]
 ]],
     help_text = [[
 ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
@@ -477,14 +477,14 @@ function create_config( )
 ------------------------------------------------------------------------------------------------------------
  نکته !!!! :   با دستور *اخراجم کن* میتوانید از گروه بروید . 
 ------------------------------------------------------------------------------------------------------------
-کانال ما : @Pro_Programing
+کانال ما : @EMAMALI_PRO
 
 اعضای اصلی ما : 
 
-@Arsalan_Sahel  [سازنده]
-@Bad00boy_Creed [عضو اصلی ]
-@Creed_aria [عضو اصلی]
-@AmirCc_CreeD [عضو اصلی]
+@SOLTANI330  [سازنده]
+@SOLTANI330 [عضو اصلی]
+@SOLTANI330 [عضو اصلی]
+@SOLTANI330 [عضو اصلی]
 ]],
   }
   serialize_to_file(config, './data/config.lua')
